8c0403f384f283fa64a7d771341728b68cb48e00
JLDeveloper <Jonathan.lawrence215@gmail.com>
2017-03-05 21:26:20 -0600

    Enhanced Skills Tree

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,6 +1,11 @@
-$(function(){
-    $("").tooltip();
-  });
-  $("#html5-logo").hover(function(){
-      $('.HTML5').slideToggle();
-  });
+/*
+$("#html5-logo").mouseover(function() {
+	$(".hidden").css('visibility','visible');
+ });
+$(".hidden").mouseover(function() {
+	$(".hidden").css('visibility','visible');
+ });
+$(".hidden").mouseout(function() {
+	$(".hidden").css('visibility','hidden');
+ });
+ */
