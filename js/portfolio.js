3d5c617b2a6f7494968529d3dc14292423825293
JLDeveloper <Jonathan.lawrence215@gmail.com>
2017-03-29 20:27:08 -0500

    Modified hover for left hand nav

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,4 +1,5 @@
 $(function(){ // DOM ready
+
  //Skill Glow
  $( ".skill" ).mouseenter(function() {
   $( this ).animate({boxShadow: '0 0 30px #00cc00'}, 200 );
@@ -6,12 +7,13 @@ $(function(){ // DOM ready
  $( ".skill" ).mouseleave(function() {
   $( this ).animate({boxShadow: '0 0 0 #000000'}, 200 );
  });
+
  //Show Email Address
  $("#emailicon").click(function () {
      $("#email").fadeOut(function () {
        $("#email").text('jonathan.lawrence215@gmail.com').fadeIn();
-     })
- })
+     });
+ });
 });
 
 /*
